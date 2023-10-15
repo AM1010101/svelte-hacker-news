30f36871e3e53e68e9746c2583216e0380b70a7b
Andrew Merrin <78210129+AM1010101@users.noreply.github.com>
2023-10-15 18:58:50 +0100

    implement many story types

--- src/lib/Components/hnPromise.ts
+++ src/lib/Components/hnPromise.ts
@@ -1,49 +1,100 @@
-
-
 interface Story {
-    by: string;
-    descendants: number;
-    id: number;
-    kids: number[];
-    score: number;
-    time: number;
-    title: string;
-    type: string;
-    url: string;
-  }
-
-interface Comment {
-    by: string;
-    id: number;
-    kids: number[];
-    parent: number;
-    text: string;
-    time: number;
-    type: string;
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
 }
 
-export async function hnStoryPromise(id: number) {
-    return fetch(
-      `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        return data as Story;
-      })
-      .catch((err) => {
-        console.log('err', err);
-      });
-  }
+interface Comment {
+  by: string;
+  id: number;
+  kids: number[];
+  parent: number;
+  text: string;
+  time: number;
+  type: string;
+}
+
+export let hnTopStorys = fetch(
+  `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    return data;
+  });
+
+export let hnNewStorys = fetch(
+  `https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    return data;
+  });
+
+export let hnBestStorys = fetch(
+  `https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    return data;
+  });
+
+export let hnAskStorys = fetch(
+  `https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    return data;
+  });
+
+export let hnShowStorys = fetch(
+  `https://hacker-news.firebaseio.com/v0/showstories.json?print=pretty`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    return data;
+  });
+
+export let hnJobStorys = fetch(
+  `https://hacker-news.firebaseio.com/v0/jobstories.json?print=pretty`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    return data;
+  });
+
+export let hnMaxItem = fetch(
+  `https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    return data;
+  });
+
+export async function hnStoryPromise(id: number): Promise<Story> {
+  return fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      return data as Story;
+    });
+}
 
 export async function hnCommentPromise(id: number) {
-    return fetch(
-      `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        return data as Comment;
-      })
-      .catch((err) => {
-        console.log('err', err);
-      });
-  }
+  return fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      return data as Comment;
+    })
+    .catch((err) => {
+      console.log('err', err);
+    });
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,34 +3,57 @@
   import { fly } from 'svelte/transition';
   import { cubicOut } from 'svelte/easing';
   import ThemeToggle from '../lib/Components/ThemeToggle.svelte';
+  import {
+    hnTopStorys,
+    hnAskStorys,
+    hnShowStorys,
+    hnJobStorys,
+    hnNewStorys,
+  } from '../lib/Components/hnPromise';
 
   let paginationSive = 50;
   let itemsToLoad = 50;
-
   function loadMore() {
     itemsToLoad += paginationSive;
   }
 
-  let hnPromise = fetch(
-    'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log('data', data);
-      return data;
-    })
-    .catch((err) => {
-      console.log('err', err);
-    });
+  let category = hnTopStorys;
+  let categories = ['Top', 'Ask', 'Show', 'Jobs', 'New'];
+  let selectedCategory = 'Top';
+
+  let categoryFunctions: any = {
+    Top: hnTopStorys,
+    Ask: hnAskStorys,
+    Show: hnShowStorys,
+    Jobs: hnJobStorys,
+    New: hnNewStorys,
+  };
+
+  $: {
+    category = categoryFunctions[selectedCategory] || hnTopStorys;
+  }
 </script>
 
 <div class="flex justify-between items-center p-2">
-  <h1 class="text-3xl font-bold underline">Hacker News App!</h1>
+  <h1 class="text-3xl font-bold underline">HN Stories</h1>
   <ThemeToggle themes={['light', 'hacker']} />
 </div>
 
+<div class="flex flex-row tabs flex-1 justify-evenly">
+  {#each categories as category (category)}
+    <button
+      class="tab tab-lifted flex-1 {selectedCategory === category
+        ? 'tab-active'
+        : ''}"
+      on:click={() => (selectedCategory = category)}
+    >
+      {category}
+    </button>
+  {/each}
+</div>
+
 <div class="items-center w-full">
-  {#await hnPromise}
+  {#await category}
     <span class="loading loading-infinity loading-md" />
   {:then hn}
     <ul class="m-2">
